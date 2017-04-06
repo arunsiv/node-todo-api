e821d1fcfa8526c97fc975a45fea725f94d9019a
arunsiv <arunsiv>
2017-04-06 10:53:45 -0400

    New files from github

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,29 +1,35 @@
-const {MongoClient} = require('mongodb');
+// const MongoClient = require('mongodb').MongoClient;
+//ES6 Object Destructuring
+const {MongoClient, ObjectID} = require('mongodb');
+
+//Generating object ID from Mongo DB
+// var obj = new ObjectID;
+// console.log(obj);
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     if (err) {
         return console.log('Unable to connect to MongoDB');
     }
-    console.log('Connected to MongoDB...');
+    console.log('Connected to MongoDb server');
 
     db.collection('Todos').insertOne({
-        todo: 'Play Volleyball',
+        todo: 'Buy Groceries',
         completed: false
     }, (err, result) => {
         if (err) {
-            return console.log('Unable to Insert', err);
+            return console.log('Unable to insert todos', err);
         }
 
         console.log(JSON.stringify(result.ops, undefined, 4));
     });
 
     db.collection('Users').insertOne({
-        name: 'John',
+        name: 'Arun',
         age: 32,
-        location: 'Galati'
+        location: 'home'
     }, (err, result) => {
         if (err) {
-            return console.log('Unable to Insert', err);
+            return console.log('Unable to insert users', err);
         }
 
         console.log(JSON.stringify(result.ops, undefined, 4));
--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -6,20 +6,22 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
     console.log('Connected to MongoDB...');
 
-    // db.collection('Todos').find({
-    //     _id: new ObjectID('58e50654d2786527f8d75e08')
-    // }).toArray().then((docs) => {
-    //     console.log('Todos');
-    //     console.log(JSON.stringify(docs, undefined, 4));
-    // }, (err) => {
-    //     console.log('Unable to fetch Todos', err);
-    // });
+    //*** find
+    db.collection('Todos').find({
+        _id: new ObjectID('58e50654d2786527f8d75e08')
+    }).toArray().then((docs) => {
+        console.log('Todos');
+        console.log(JSON.stringify(docs, undefined, 4));
+    }, (err) => {
+        console.log('Unable to fetch Todos', err);
+    });
 
-    // db.collection('Todos').count().then((docs) => {
-    //     console.log(`Todos Count: ${docs}`);
-    // }, (err) => {
-    //     console.log('Unable to fetch Todos', err);
-    // });
+    //*** get count
+    db.collection('Todos').count().then((docs) => {
+        console.log(`Todos Count: ${docs}`);
+    }, (err) => {
+        console.log('Unable to fetch Todos', err);
+    });
 
     db.collection('Users').find({
         age: 32
