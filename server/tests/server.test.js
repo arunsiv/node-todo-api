8dff3645cd72c955568576205a0a35d1508134dd
arunsiv <arunsiv>
2017-04-10 17:29:25 -0400

    added PATCH testcases

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -11,7 +11,9 @@ const todoTestData = [{
     todo: 'Test Data 1'
 }, {
     _id: new ObjectID,
-    todo: 'Test Data 2'
+    todo: 'Test Data 2',
+    completed: true,
+    completedAt: 111
 }, {
     _id: new ObjectID,
     todo: 'Test Data 3'
@@ -159,6 +161,68 @@ describe('DELETE /todos/:id', () => {
     it('should return 404 for invalid ids', (done) => {
         request(app)
             .delete('/todos/123xyz')
+            .expect(404)
+            .end(done);
+    });
+});
+
+describe('PATCH /todos/:id', () => {
+    var hexId = new ObjectID(todoTestData[1]._id).toHexString();
+
+    it('should update the todo', (done) => {
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toNotExist();
+                    done();
+                }).catch((e) => {
+                    done(e);
+                });
+            });
+    });
+
+    it('should clear completedAt when todo is not completed', (done) => {
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toNotExist();
+                    done();
+                }).catch((e) => {
+                    done(e);
+                });
+            });
+    });
+
+    it('should return 404 if todo is not found', (done) => {
+        var id = new ObjectID().toHexString();
+
+        request(app)
+            .patch(`/todos/${id}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 for invalid ids', (done) => {
+        request(app)
+            .patch('/todos/123xyz')
             .expect(404)
             .end(done);
     });
