8eb9b2c0c3a64c1f37acba42ca62cdce6b4b9d81
arunsiv <arunsiv>
2017-04-12 13:05:43 -0400

    added test case for DELETE /users/me/token route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -362,3 +362,31 @@ describe('POST /users/login', () => {
     });
 
 });
+
+describe('DELETE /users/me/token', () => {
+
+    it('should delete auth token on logout', (done) => {
+        var token = userTestData[0].tokens[0].token;
+        var email = userTestData[0].email;
+
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findOne({
+                    email
+                }).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => {
+                    done(e);
+                });
+            });
+    });
+
+});
